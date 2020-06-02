e23297cbacf0303d217fb46bfc04b5d84d03e26d
JulianoV <ventolajuliano@gmail.com>
2020-06-01 22:02:33 -0300

    com: Adicionado comentarios as linhas de codigo

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 
+// Cria um servidor com express
 const app = express();
 
+// Cria uma resposta para rota "/"
 app.get("/", (request, response) => {
-  return response.send("Running!");
+  return response.json({ message: "Hello world!" });
 });
 
+// Mantem o servidor escutando na porta 3333
 app.listen(3333, () => console.log(`Server running on port 3333`));
