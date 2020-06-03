69ca705038aa519c236c82291c5f8f6eeab24347
JulianoV <ventolajuliano@gmail.com>
2020-06-02 23:14:02 -0300

    feat: Rota estatica para mostrar imagens

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import routes from "./routes";
 
 // Cria um servidor com express
@@ -7,7 +8,12 @@ const app = express();
 // Coloca interpretador de JSON para as requisições
 app.use(express.json());
 
+// Seta a utilização de rotas
 app.use(routes);
+
+// Seta a rota '/upload' para imagens estáticas(publicas)
+// ex: http://localhost:3333/uploads/lampadas.svg
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 // Mantem o servidor escutando na porta 3333
 app.listen(3333, () => console.log(`Server running on port 3333`));
