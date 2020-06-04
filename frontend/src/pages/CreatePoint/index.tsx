0c6eaba608d3a6bcda270fb63466c2849b34e159
JulianoV <ventolajuliano@gmail.com>
2020-06-04 00:33:11 -0300

    feat: Cadastro de pontos de coleta

--- frontend/src/pages/CreatePoint/index.tsx
+++ frontend/src/pages/CreatePoint/index.tsx
@@ -1,7 +1,111 @@
 import React from "react";
+import { FiArrowLeft } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.svg";
+
+import "./styles.css";
 
 const CreatePoint: React.FC = () => {
-  return <h1>Create Point</h1>;
+  return (
+    <div id="page-create-point">
+      <header>
+        <img src={logo} alt="Logo Ecoleta" />
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para home
+        </Link>
+      </header>
+
+      {/* --------------------------- FORM------------------------- */}
+      <form>
+        <h1>
+          Cadasto do
+          <br /> ponto de coleta
+        </h1>
+        {/* --------------------------- DADOS------------------------- */}
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+          <div className="field">
+            <label htmlFor="name">Nome da entidade</label>
+            <input type="text" name="name" id="name" />
+          </div>
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="email">E-mail</label>
+              <input type="email" name="email" id="email" />
+            </div>
+            <div className="field">
+              <label htmlFor="whatsapp">Whatsapp</label>
+              <input type="text" name="whatsapp" id="whatsapp" />
+            </div>
+          </div>
+        </fieldset>
+        {/* --------------------------- ENDEREÇO------------------------- */}
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+            <span>Selecione o endereço no mapa</span>
+          </legend>
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="uf">Estado(UF)</label>
+              <select name="uf" id="uf">
+                <option value="0">Selecione uma UF</option>
+              </select>
+            </div>
+            <div className="field">
+              <label htmlFor="city">Cidade</label>
+              <select name="city" id="city">
+                <option value="0">Selecione uma cidade</option>
+              </select>
+            </div>
+          </div>
+          <div className="field">
+            <label htmlFor="number">Número</label>
+            <input type="number" min="1" name="number" id="number" />
+          </div>
+        </fieldset>
+        {/* --------------------------- ITENS DE COLETA-------------------- */}
+        <fieldset>
+          <legend>
+            <h2>Ítems de coleta</h2>
+            <span>Selecione um ou mais ítens abaixo</span>
+          </legend>
+
+          <ul className="items-grid">
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="oleo" />
+              <span>Óleo de cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="oleo" />
+              <span>Óleo de cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="oleo" />
+              <span>Óleo de cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="oleo" />
+              <span>Óleo de cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="oleo" />
+              <span>Óleo de cozinha</span>
+            </li>
+            <li>
+              <img src="http://localhost:3333/uploads/oleo.svg" alt="oleo" />
+              <span>Óleo de cozinha</span>
+            </li>
+          </ul>
+        </fieldset>
+
+        <button type="submit">Cadastrar ponto de coleta</button>
+      </form>
+    </div>
+  );
 };
 
 export default CreatePoint;
