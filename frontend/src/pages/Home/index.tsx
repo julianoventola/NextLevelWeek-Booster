4db56d028463d2502e37e95fdf4bec31c6b9b774
JulianoV <ventolajuliano@gmail.com>
2020-06-04 00:03:47 -0300

    feat: Alterações no componente Home

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { FiLogIn } from "react-icons/fi";
+
+import "./styles.css";
+
+import logo from "../../assets/logo.svg";
 
 const Home: React.FC = () => {
-  return <h1>Home</h1>;
+  return (
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={logo} alt="Logo Ecoleta" />
+        </header>
+
+        <main>
+          <h1>Seu marketplace de coleta de resíduos</h1>
+          <p>
+            Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+          </p>
+
+          <a href="/cadastro">
+            <span>
+              <FiLogIn />{" "}
+            </span>{" "}
+            <strong>Cadastre um ponto de coleta</strong>
+          </a>
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
