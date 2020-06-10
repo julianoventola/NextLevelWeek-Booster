e728e0dc46c2ef292d33ec081344562f705b0674
JulianoV <ventolajuliano@gmail.com>
2020-06-10 00:16:47 -0300

    feat: Validação de campos na rota post

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { celebrate, Joi } from "celebrate";
 
 import multer from "multer";
 import multerConfig from "./config/multer";
@@ -15,8 +16,30 @@ const upload = multer(multerConfig);
 routes.get("/items", ItemsController.index);
 
 //Rotas de Pontos de coleta
-routes.post("/points", upload.single("image"), PointsController.create);
 routes.get("/points/:id", PointsController.show);
 routes.get("/points", PointsController.index);
+routes.post(
+  "/points",
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+        number: Joi.number().required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    }
+  ),
+  upload.single("image"),
+  PointsController.create
+);
 
 export default routes;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import routes from "./routes";
+import { errors } from "celebrate";
 
 // Cria um servidor com express
 const app = express();
@@ -18,6 +19,9 @@ app.use(routes);
 // Seta a rota '/upload' para imagens estáticas(publicas)
 // ex: http://localhost:3333/uploads/lampadas.svg
 app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+
+// Utiliza a validação de erros
+app.use(errors());
 
 // Mantem o servidor escutando na porta 3333
 app.listen(3333, () => console.log(`Server running on port 3333`));
