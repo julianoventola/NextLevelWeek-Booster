b3511de14717fd5eb0b07dc1b1a1d5563e982b7d
JulianoV <ventolajuliano@gmail.com>
2020-06-09 23:08:09 -0300

    chore: Configuração multer para envio de arquivos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,15 +1,21 @@
 import express from "express";
+
+import multer from "multer";
+import multerConfig from "./config/multer";
+
 import PointsController from "./controllers/PointsController";
 import ItemsController from "./controllers/ItemsController";
 
 // Cria o "roteador"
 const routes = express.Router();
 
+const upload = multer(multerConfig);
+
 // Rotas de items de coleta
 routes.get("/items", ItemsController.index);
 
 //Rotas de Pontos de coleta
-routes.post("/points", PointsController.create);
+routes.post("/points", upload.single("image"), PointsController.create);
 routes.get("/points/:id", PointsController.show);
 routes.get("/points", PointsController.index);
 
